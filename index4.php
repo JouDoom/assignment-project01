ff7c071931062253f2bd4e0e197d8c82af2aad3d
Jou Doom <91768022+JouDoom@users.noreply.github.com>
2022-03-26 11:01:48 +0900

    various conditional

---
+++ index4.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Jou Doom</title>
+</head>
+
+<body>
+
+<?php
+
+   /* //Arimethic Operators
+    echo 5**2 ;
+    echo 8/4;*/
+
+    /*//Assignment Operators
+    $x = 524;
+    $x -= 43;
+
+
+    echo $x;*/
+
+    /*//Comparison Operators
+    $x = 5;
+    $y = 10;
+
+    if ($x > $y) {
+        echo "True!";
+    }
+    else {
+        echo "False!";
+    }*/
+
+
+    /*//Increment/Decrement Operators
+    $x = 10;
+    echo $x--;
+    echo $x;*/
+
+
+    //Logical Operators
+    $x = 10;
+    $y = 20;
+
+    if ($x == $y || 1 == 1) {
+        echo "True";
+    }
+
+    $x = 1;
+
+    if ($x == 1) {
+        echo "Daniel is very handsome!";
+    } else {
+        echo "Daniel is very handsome!";
+    }
+
+    
+
+?>
+    
+</body>
+
+
+
+</html>
+
+
